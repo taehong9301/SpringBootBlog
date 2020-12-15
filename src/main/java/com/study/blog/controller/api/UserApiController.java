d1cd6e77c5df0cda977f532e5e12db581f752479
taehong.kim <public.devhong@gmail.com>
2020-12-15 22:12:27 +0900

    update

--- src/main/java/com/study/blog/controller/api/UserApiController.java
+++ src/main/java/com/study/blog/controller/api/UserApiController.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
-
 @Slf4j
 @RestController
 @RequestMapping("api")
@@ -17,7 +15,8 @@ public class UserApiController {
 
   @Autowired private UserService userService;
 
-  @Autowired private HttpSession session;
+  // 전통적인 로그인 방식 ( 사용 안함 )
+  //  @Autowired private HttpSession session;
 
   @PostMapping("user")
   public @ResponseBody ResponseDto<User> user(@RequestBody User user) {
@@ -28,14 +27,15 @@ public class UserApiController {
     }
   }
 
-  @PostMapping("user/login")
-  public @ResponseBody ResponseDto<Integer> userLogin(@RequestBody User user) {
-    try {
-      User principal = userService.login(user);
-      session.setAttribute("principal", principal);
-      return new ResponseDto<>(HttpStatus.OK.value(), 1);
-    } catch (Exception e) {
-      return new ResponseDto<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), null);
-    }
-  }
+  // 전통적인 로그인 방식 ( 사용 안함 )
+  //  @PostMapping("user/login")
+  //  public @ResponseBody ResponseDto<Integer> userLogin(@RequestBody User user) {
+  //    try {
+  //      User principal = userService.login(user);
+  //      session.setAttribute("principal", principal);
+  //      return new ResponseDto<>(HttpStatus.OK.value(), 1);
+  //    } catch (Exception e) {
+  //      return new ResponseDto<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), null);
+  //    }
+  //  }
 }
